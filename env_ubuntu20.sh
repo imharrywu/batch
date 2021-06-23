04ded262d70559544a3e24e611c302aa9a63a561
HarryWu <imharrywu@users.noreply.github.com>
2021-06-23 08:25:12 +0800

    Create env_ubuntu20.sh

---
+++ env_ubuntu20.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt-get update -qy
+sudo apt-get install -qy resolvconf
+sudo systemctl enable resolvconf
+sudo systemctl start resolvconf
+sudo systemctl status resolvconf
+sudo resolvconf --enable-updates
+
+cat <<heredoc >>/etc/resolvconf.d/ressolvconf.d/head
+nameserver 1.1.1.1
+nameserver 8.8.8.8
+heredoc
+
+sudo resolvconf -u
+
+sudo apt-get install -qy curl git jq gcc g++ nodejs make build-essential docker.io resolvconf
+
+#wget https://golang.google.cn/dl/go1.16.5.linux-amd64.tar.gz
+#tar zxvf ./go1.16.5.linux-amd64.tar.gz
+
+export GOROOT=/root/go
+export GOPATH=/root/gopath
+export PATH=$GOROOT/bin:$PATH
